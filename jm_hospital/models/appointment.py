09b742d6b8a778b867091d9edf6fb9ba9171b094
jmlm74 <jmlm74@gmail.com>
2021-02-05 17:04:15 +0100

    How To Set Default Value For The Field

--- jm_hospital/models/appointment.py
+++ jm_hospital/models/appointment.py
@@ -16,9 +16,12 @@ class HospitalAppointment(models.Model):
         result = super(HospitalAppointment, self).create(vals)
         return result
 
+    def _get_default_note(self):
+        return "Default Value"
+
     name = fields.Char(string='Appointment ID', required=True, copy=False, readonly=True,
                        index=True, default=lambda self: _('New'))
     patient_id = fields.Many2one('hospital.patient', string='Patient', required=True)
     patient_age = fields.Integer('Age', related='patient_id.patient_age')
-    notes = fields.Text(string="Registration Note")
+    notes = fields.Text(string="Registration Note", default=_get_default_note)
     appointment_date = fields.Date(string='Date', required=True)
