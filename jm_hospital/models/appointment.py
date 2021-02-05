3afc9b8d8833109797c4e76ed98f618b1ee9713d
jmlm74 <jmlm74@gmail.com>
2021-02-05 21:50:54 +0100

    How to Add States/Statusbar

--- jm_hospital/models/appointment.py
+++ jm_hospital/models/appointment.py
@@ -25,3 +25,8 @@ class HospitalAppointment(models.Model):
     patient_age = fields.Integer('Age', related='patient_id.patient_age')
     notes = fields.Text(string="Registration Note", default=_get_default_note)
     appointment_date = fields.Date(string='Date', required=True)
+    state = fields.Selection([ ('draft', 'Draft'),
+                               ('confirm', 'Confirm'),
+                               ('done', 'Done'),
+                               ('cancel', 'Cancelled'),
+                              ], string='Status', readonly=True, default='draft')
