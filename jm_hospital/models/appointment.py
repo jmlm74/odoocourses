f5cc8c8cab7dc082d8a5abbd4116de579a28b781
jmlm74 <jmlm74@gmail.com>
2021-02-06 15:56:00 +0100

    Control States and Statusbar Using Buttons

--- jm_hospital/models/appointment.py
+++ jm_hospital/models/appointment.py
@@ -25,8 +25,30 @@ class HospitalAppointment(models.Model):
     patient_age = fields.Integer('Age', related='patient_id.patient_age')
     notes = fields.Text(string="Registration Note", default=_get_default_note)
     appointment_date = fields.Date(string='Date', required=True)
-    state = fields.Selection([ ('draft', 'Draft'),
-                               ('confirm', 'Confirm'),
-                               ('done', 'Done'),
-                               ('cancel', 'Cancelled'),
-                              ], string='Status', readonly=True, default='draft')
+    state = fields.Selection([('draft', 'Draft'),
+                              ('confirm', 'Confirm'),
+                              ('done', 'Done'),
+                              ('cancel', 'Cancelled'),
+                             ], string='Status', readonly=True, default='draft')
+
+    # Moving the State Of the Record To Confirm State in Button Click
+    # How to Add States/Statusbar for Records in Odoo
+    # https://www.youtube.com/watch?v=lPHWsw3Iclk&list=PLqRRLx0cl0hoJhjFWkFYowveq2Zn55dhM&index=21
+    def action_confirm(self):
+        for rec in self:
+            rec.state = 'confirm'
+            return {
+                'effect': {
+                    'fadeout': 'slow',
+                    'message': 'Appointment Confirmed... Thanks You',
+                    'type': 'rainbow_man',
+                }
+            }
+
+    def action_done(self):
+        for rec in self:
+            rec.state = 'draft'
+
+    def action_notify(self):
+        for rec in self:
+            rec.doctor_id.user_id.notify_warning(message='Appointment is Confirmed')
