d0895ded1a9320f5bea6f952d695d0abc92f1d3f
jmlm74 <jmlm74@gmail.com>
2021-02-14 17:25:39 +0100

    How TO Add Banner route

--- jm_hospital/controllers/main.py
+++ jm_hospital/controllers/main.py
@@ -3,6 +3,22 @@ from odoo.http import request
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
+class AppointmentController(http.Controller):
+
+    @http.route('/jm_hospital/appointments', auth='user', type='json')
+    def appointment_banner(self):
+        return {
+            'html': """
+                    <div>
+                        <link>
+                        <center><h1><font color="red">Subscribe the channel.......!</font></h1></center>
+                        <center>
+                        <p><font color="blue"><a href="https://www.youtube.com/channel/UCVKlUZP7HAhdQgs-9iTJklQ/videos">
+                            Get Notified Regarding All The Odoo Updates!</a></p>
+                            </font></div></center> """
+                                }
+
+
 class WebsiteSaleInherit(WebsiteSale):
     # 1st install website_sale !
     # override shop method --> to show How-to
